4fa494fe8d2ca1be60ffe65207ca44e1840f967b
Canttuchdiz <75583497+Canttuchdiz@users.noreply.github.com>
2023-03-03 19:01:29 -0500

    Update config.py

--- rojitzu/utils/config.py
+++ rojitzu/utils/config.py
@@ -3,5 +3,5 @@ from discord.types.snowflake import Snowflake
 
 class Config:
 
-    log_channel_id: Snowflake = 1078876621752238120
+    log_channel_id: Snowflake = 1081365719742169312
     ticket_cat_id: Snowflake = 1075550668661719110
